e20c1e3feb9f6634b82262d28d89621af4ed4622
Andrew Van Uitert <avanuitert@control4.com>
2017-03-08 09:24:57 -0700

    Adding auto commands

--- src/org/usfirst/frc5933/Caroline/SocketVision.java
+++ src/org/usfirst/frc5933/Caroline/SocketVision.java
@@ -1,7 +1,5 @@
-//TODO: Make duplicate socket at a different port to take input from
-package org.usfirst.frc5933.Caroline; //the second target. Need two string parses and to ACTUALLY USE THE
 
-//CODE SOMEWHERE ELSE! Driver assist? Auto commands? Mayonnaise?
+package org.usfirst.frc5933.Caroline;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
